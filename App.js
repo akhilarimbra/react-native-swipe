6b459560ab534b6c87e15ae1ae5cc5a20859ff35
Akhil Raj Arimbra <akhilarimbra@Akhils-MacBook-Pro.local>
2019-08-20 15:58:01 +0530

    code reformat

--- App.js
+++ App.js
@@ -4,13 +4,11 @@ import { StyleSheet, Text, View } from 'react-native'
 import Ball from './src/Ball'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Ball />
-      </View>
-    )
-  }
+  render = () => (
+    <View style={styles.container}>
+      <Ball />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
